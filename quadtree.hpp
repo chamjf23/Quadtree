b0b8a417caf9f860f43f2f27d455f6a6c1acf879
chamjf23 <63682727+chamjf23@users.noreply.github.com>
2020-10-25 19:14:05 -0500

    Avance

--- quadtree.hpp
+++ quadtree.hpp
@@ -11,31 +11,35 @@ using namespace std;
 struct Point {
   int x;
   int y;
+  Point(){x=0; y=0;}//constructor
+  Point(int cordenanda_x, int cordenanda_y){x=cordenanda_x; y = cordenanda_y}
 };
 
 //nodo con el cual va a funcionar el quadtree
 template <typename T>
 struct QuadNode {
-  Point pos;
-  T data;
-  QuadNode<T> *top_left;
-  QuadNode<T> *close_left;
-  QuadNode<T> *close_right;
-  QuadNode<T> *top_right;
+  rectan perimetro;
+  int capacidad = 5;
+  Point *puntos = new vector<Point>;
+  bool divided = false;
+  
 };
 
-//crear el rectangulo donde funcionar el QuadTree
+//crear el rectangulo donde funcionara el QuadTree
 class rectan{
 private:
+  float c_x;
+  float c_y;
   int altura;
   int ancho;
 public:
-  int get_altura();
-  int get_ancho();
-  rectan(){altura=10; int ancho=10;}
-  void print_rec(rectan a);
-
-
+  int get_altura(){return altura;}
+  int get_ancho(){return ancho;}
+  Point get_ubicacion(){return Point(c_x, c_y);}
+  rectan(){altura=200;  ancho=200; c_x = 100; c_y = 100}
+  rectan(int alt; int anch){altura = alt; ancho = anch; c_x = anch/2; c_y = alt/2;}
+  ~rectan();
+  friend void print_rec(rectan a);
 };
 
 
@@ -44,12 +48,44 @@ template <typename T>
 class QuadTree {
 private:
   QuadNode<T> *root;
+  void insert_point(Point t);
+  void subdivide();
+public:
+  QuadTree();
+  QuadTree(rectan perimetro, int capacidad);
+  ~QuadTree();
+
+  
 };
 
 
+
+
+void QuadTree::insert_point(Point t){
+  if (root->puntos.size() < root->capacidad)
+  {
+    root->puntos.push_back(t)
+  }else{
+    subdivide{
+    }
+  }
+}
+
+
 void rectan::print_rec(rectan a){
-    for(int i=0; i<a.ancho;i++)
+    for(int i=0; i<a.ancho;i++){
       cout<<"*";
+    }
+    for (int i=0; i<a.altura; i++){
+      cout<<endl;
+      if (i==0 || i == a.altura-1){
+        cout<<'*';
+      }
+    }
+    for (int i=0; i<a.ancho; i++){
+      cout<<'*';
+    }
+    cout<<endl;
    }
 
 
